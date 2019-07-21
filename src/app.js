aef16616c605ea3c09984960cde31e35820d7604
SimasDei <simondeiman@gmail.com>
2019-07-21 17:41:57 +0300

    restructure app.js into separate modules

--- src/app.js
+++ src/app.js
@@ -1,44 +1,19 @@
-const numberOneInput = document.getElementById('numberOne');
-const numberTwoInput = document.getElementById('numberTwo');
-const addValuesButton = document.getElementById('addValues');
-const resultDiv = document.getElementById('result');
-const errorBox = document.getElementById('error');
+const alertService = new AlertService();
+const componentService = new ComponentService();
+const run = (alertService, componentService) => {
+  alertService.hideErrors();
 
-const parseInputs = (...input) => {
-  return input.map(str => parseInt(str));
-};
-
-const inputsAreValid = (...input) => {
-  return input.every(num => typeof num === 'number' && !isNaN(num));
-};
-
-const handleAdditionError = (inputs, numbers) => {
-  const fullMessage = inputs.reduce((message, str, index) => {
-    if (inputsAreValid(numbers[index])) {
-      return message + '';
+  componentService.onClick(() => {
+    alertService.hideErrors();
+    const inputs = componentService.getInputs();
+    const parsedInputs = parseInputs(...inputs);
+    if (inputsAreValid(...parsedInputs)) {
+      const [numA, numB] = parsedInputs;
+      componentService.setResult(numA + numB);
     } else {
-      return message + `${str} is not a number. `;
+      componentService.setResult('');
+      alertService.handleAdditionError(inputs, parsedInputs);
     }
-  }, 'Please enter two valid numbers! ');
-
-  errorBox.classList.remove('invisible');
-  errorBox.innerText = fullMessage;
+  });
 };
-
-const hideErrors = () => {
-  errorBox.classList.add('invisible');
-};
-
-hideErrors();
-addValuesButton.addEventListener('click', () => {
-  hideErrors();
-  const inputs = [numberOneInput.value, numberTwoInput.value];
-  const parsedInputs = parseInputs(...inputs);
-  if (inputsAreValid(...parsedInputs)) {
-    const [numA, numB] = parsedInputs;
-    resultDiv.innerText = numA + numB;
-  } else {
-    resultDiv.innerText = '';
-    handleAdditionError(inputs, parsedInputs);
-  }
-});
+run(alertService, componentService);
