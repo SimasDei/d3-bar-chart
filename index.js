75fee5435240cc85d9b771ecf616c172259493a4
Simas <simonas.deimantavicius@mediapark.com>
2019-07-29 15:28:23 +0300

    use min max and extent

--- index.js
+++ index.js
@@ -2,37 +2,39 @@
 const svg = d3.select('svg');
 
 d3.json('menu.json').then(data => {
-  const y = d3
-    .scaleLinear()
-    .domain([0, 1000])
+
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.orders)])
     .range([0, 500]);
 
-  const x = d3
-    .scaleBand()
+  const x = d3.scaleBand()
     .domain(data.map(item => item.name))
     .range([0, 500])
     .paddingInner(0.2)
     .paddingOuter(0.2);
 
-  console.log(x('veg burger'));
-  console.log(x('veg curry'));
+  // const min = d3.min(data, d => d.orders);
+  // const max = d3.max(data, d => d.orders);
+  // const extent = d3.extent(data, d => d.orders);
+
+  // console.log(min, max, extent);
 
   // join the data to circs
-  const rects = svg.selectAll('rect').data(data);
+  const rects = svg.selectAll('rect')
+    .data(data);
 
   // add attrs to circs already in the DOM
-  rects
-    .attr('width', x.bandwidth)
-    .attr('height', d => y(d.orders))
+  rects.attr('width', x.bandwidth)
+    .attr("height", d => y(d.orders))
     .attr('fill', 'orange')
     .attr('x', d => x(d.name));
 
   // append the enter selection to the DOM
-  rects
-    .enter()
+  rects.enter()
     .append('rect')
-    .attr('width', x.bandwidth)
-    .attr('height', d => y(d.orders))
-    .attr('fill', 'orange')
-    .attr('x', d => x(d.name));
+      .attr('width', x.bandwidth)
+      .attr("height", d => y(d.orders))
+      .attr('fill', 'orange')
+      .attr('x', (d) => x(d.name));
+
 });
